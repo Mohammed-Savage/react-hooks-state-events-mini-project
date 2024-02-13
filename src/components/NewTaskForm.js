1580c179af87ddd4ed9bd68fd1e4b81c14760082
Mohammed Omar <omartechmohammed@gmail.com>
2024-02-13 16:36:09 -0500

    completed my lab

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+
+  const [details, setDetails] = useState("")
+  const [category, setCategory] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const newTask = { text: details, category: category }; // newTask = setDetails + setCategory
+    onTaskFormSubmit(newTask); // submit newTask to the COPY, NOT the task from the data.js
+  }
+
   return (
-    <form className="new-task-form">
-      <label>
-        Details
-        <input type="text" name="text" />
-      </label>
-      <label>
-        Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
-        </select>
-      </label>
-      <input type="submit" value="Add task" />
-    </form>
+    <form className="new-task-form" onSubmit={handleSubmit}>
+    <label>
+      Details
+      <input type="text" name="text" value={details} onChange={(e) => setDetails(e.target.value)} />
+    </label>
+    <label>
+      Category
+      <select name="category" value={category} onChange={(e) => setCategory(e.target.value)}>
+        {/* render <option> elements for each category here */}
+        {categories.map(category => (
+          <option key={category}>
+            {category}
+          </option>
+        ))}
+      </select>
+    </label>
+    <input type="submit" value="Add task" />
+  </form>
   );
 }
 
